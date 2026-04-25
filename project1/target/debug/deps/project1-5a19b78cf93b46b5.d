e3c8f0fe3190e80cc49ee25e7a96d21f53f10d67
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-04-25 13:27:40 -0600

    checking out rust

---
+++ project1/target/debug/deps/project1-5a19b78cf93b46b5.d
@@ -0,0 +1,5 @@
+/Users/adharsh/Desktop/FeOS/project1/target/debug/deps/project1-5a19b78cf93b46b5.d: src/main.rs
+
+/Users/adharsh/Desktop/FeOS/project1/target/debug/deps/project1-5a19b78cf93b46b5: src/main.rs
+
+src/main.rs:
---
+++ project1/target/debug/project1.d
@@ -0,0 +1,1 @@
+/Users/adharsh/Desktop/FeOS/project1/target/debug/project1: /Users/adharsh/Desktop/FeOS/project1/src/main.rs
