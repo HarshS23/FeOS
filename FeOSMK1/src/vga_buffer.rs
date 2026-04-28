031de2f455cb7523cfbd71d7a4c8d2d7bed31099
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-04-28 17:52:10 -0500

    Saving my progress so far

--- FeOSMK1/src/vga_buffer.rs
+++ FeOSMK1/src/vga_buffer.rs
@@ -9,5 +9,14 @@ pub enum Color{
     Cyan = 3, 
     Red = 4, 
     Magenta = 5, 
-    Brown = 6
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
 }
