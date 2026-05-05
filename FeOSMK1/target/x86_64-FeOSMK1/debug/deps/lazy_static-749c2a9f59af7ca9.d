516bfb8718c72afbe9c5b1cf32d4e9833984930a
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-05-05 13:48:11 -0500

    save

---
+++ FeOSMK1/target/x86_64-FeOSMK1/debug/deps/lazy_static-749c2a9f59af7ca9.d
@@ -0,0 +1,8 @@
+/Users/adharsh/Desktop/FeOS/FeOSMK1/target/x86_64-FeOSMK1/debug/deps/lazy_static-749c2a9f59af7ca9.d: /Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/core_lazy.rs
+
+/Users/adharsh/Desktop/FeOS/FeOSMK1/target/x86_64-FeOSMK1/debug/deps/liblazy_static-749c2a9f59af7ca9.rlib: /Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/core_lazy.rs
+
+/Users/adharsh/Desktop/FeOS/FeOSMK1/target/x86_64-FeOSMK1/debug/deps/liblazy_static-749c2a9f59af7ca9.rmeta: /Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/core_lazy.rs
+
+/Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs:
+/Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/core_lazy.rs:
---
+++ FeOSMK1/target/x86_64-FeOSMK1/debug/deps/volatile-69aca85c8f6d96eb.d
@@ -0,0 +1,5 @@
+/Users/adharsh/Desktop/FeOS/FeOSMK1/target/x86_64-FeOSMK1/debug/deps/volatile-69aca85c8f6d96eb.d: /Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.2.7/src/lib.rs
+
+/Users/adharsh/Desktop/FeOS/FeOSMK1/target/x86_64-FeOSMK1/debug/deps/libvolatile-69aca85c8f6d96eb.rmeta: /Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.2.7/src/lib.rs
+
+/Users/adharsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.2.7/src/lib.rs:
