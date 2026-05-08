309bfa99e728b5baaf5134937e144e5ef7c4191c
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-05-08 17:43:16 -0600

    Working on making tests, now moving on to showing test results on the terminal

---
+++ FeOSMK1/target/x86_64-FeOSMK1/debug/deps/FeOSMK1-c5155fb6ff6233cc.d
@@ -0,0 +1,6 @@
+/Users/adharsh/Desktop/FeOS/FeOSMK1/target/x86_64-FeOSMK1/debug/deps/FeOSMK1-c5155fb6ff6233cc.d: src/main.rs src/vga_buffer.rs
+
+/Users/adharsh/Desktop/FeOS/FeOSMK1/target/x86_64-FeOSMK1/debug/deps/FeOSMK1-c5155fb6ff6233cc: src/main.rs src/vga_buffer.rs
+
+src/main.rs:
+src/vga_buffer.rs:
