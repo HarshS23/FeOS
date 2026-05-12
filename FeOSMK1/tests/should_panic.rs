944c3798ef6177962fdf5f49446d41f953f33270
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-05-11 19:23:53 -0600

    FINISHED THE BARE BONES SECTION NOW CAN MOVE ONTO THE CPU INTERRUPTS SECTION

--- FeOSMK1/tests/should_panic.rs
+++ FeOSMK1/tests/should_panic.rs
@@ -1,8 +1,6 @@
 #![no_std]
 #![no_main]
-// #![feature(custom_test_frameworks)]
-// #![test_runner(test_runner)]
-// #![reexport_test_harness_main = "test_main"]
+
 
 
 use core::panic::{self, PanicInfo};
@@ -31,22 +29,3 @@ pub extern "C" fn _start() -> ! {
     exit_qemu(QEMUExitCode::Failed);
     loop {}
 }
-
-
-
-// pub fn test_runner(tests: &[&dyn Fn()]) {
-//     serial_println!("Running {} tests", tests.len());
-//     for test in tests {
-//         test();
-//         serial_println!("[test did not panic]");
-//         exit_qemu(QEMUExitCode::Failed);
-//     }
-//     exit_qemu(QEMUExitCode::Success);
-// }
-
-
-// #[test_case]
-// fn should_fail() {
-//     serial_print!("should_panic::should_fail...\t");
-//     assert_eq!(0, 1);
-// }
