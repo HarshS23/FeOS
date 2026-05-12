f8c6f59e84a8da6990b8a3a45cb6e8cfc65f187b
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-05-11 19:04:54 -0600

    MAKEFILE: SAVING COMMAND (save)

---
+++ FeOSMK1/src/lib.rs
@@ -0,0 +1,86 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+pub mod vga_buffer;
+pub mod serial;
+
+pub trait Testable {
+        fn run(&self) -> ();
+
+}
+
+impl<T> Testable for T 
+where T: Fn(), 
+{
+        fn run(&self) -> () {
+            serial_print!(" \n{} --> ", core::any::type_name::<T>()); // invoke the funciton name 
+            self(); // check if it panics or not 
+            serial_println!("RESULT: TEST PASSED");  // prints okay if it did not panic
+        }
+
+}
+
+
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+        //println!("Running Tests: {}", tests.len());
+        serial_println!("Running tests: {}", tests.len());
+        for test in tests{
+                test.run();
+        }
+
+        // the qemu exit code 
+        exit_qemu(QEMUExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> !{
+        serial_println!("FAILED \n");
+        serial_println!("Error Type: {}\n", info);
+        exit_qemu(QEMUExitCode::Failed);
+        loop{}
+}
+
+
+#[cfg(test)]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+
+/*
+
+moving qemu exit codes into lib.rs 
+
+*/
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+// setting up Qemu exit codes 
+pub enum QEMUExitCode{
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QEMUExitCode){
+        use x86_64::instructions::port::Port;
+
+        unsafe{
+                let mut port = Port::new(0xf4); // creating a new port at oxf4
+                port.write(exit_code as u32);
+
+        }
+}
+
--- FeOSMK1/src/main.rs
+++ FeOSMK1/src/main.rs
@@ -1,7 +1,8 @@
+#![no_std] // dont link the rust standard library
+#![no_main] // disable all rust level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(FeOSMK1::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
 
 
 /*
@@ -43,38 +44,22 @@ return:
 */
 
 
-#![no_std] // dont link the rust standard library
-
-#![no_main] // disable all rust level entry points
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-// setting up Qemu exit codes 
-pub enum QEMUExitCode{
-        Success = 0x10,
-        Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QEMUExitCode){
-        use x86_64::instructions::port::Port;
-
-        unsafe{
-                let mut port = Port::new(0xf4); // creating a new port at oxf4
-                port.write(exit_code as u32);
-
-        }
-}
-
-
-
-// making a custom testing frame work 
-
-// modules --> like #include in C/C++
-mod vga_buffer;
-mod serial;
-
-
 use core::panic::PanicInfo;
+use FeOSMK1::println;
+
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World {}", "!");
+
+    #[cfg(test)]
+    test_main();
+    //panic!("some panic message");
+    loop {} //- this is unreachable after the panic happens 
+
+}
+
 
 #[cfg(not(test))]  // for not test 
 #[panic_handler]
@@ -87,62 +72,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)] // for test
 #[panic_handler]
 fn panic(info: &PanicInfo) -> !{
-        serial_println!("FAILED \n");
-        serial_println!("Error Type: {}\n", info);
-        exit_qemu(QEMUExitCode::Failed);
-        loop{}
-}
-
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-        //println!("Running Tests: {}", tests.len());
-        serial_println!("Running tests: {}", tests.len());
-        for test in tests{
-                test.run();
-        }
-
-        // the qemu exit code 
-        exit_qemu(QEMUExitCode::Success);
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    println!("This is a floating point number: {}", 5.12980);
-    #[cfg(test)]
-    test_main();
-    //panic!("some panic message");
-    loop {} //- this is unreachable after the panic happens 
-}
-
-
-
-//  dont want to have multiple serial_printline everywhere 
-// so its better to make it automatic
-pub trait Testable {
-        fn run(&self) -> ();
-
-}
-
-impl<T> Testable for T 
-where T: Fn(), 
-{
-        fn run(&self) -> () {
-            serial_print!(" \n{} --> ", core::any::type_name::<T>()); // invoke the funciton name 
-            self(); // check if it panics or not 
-            serial_println!("RESULT: TEST PASSED");  // prints okay if it did not panic
-        }
-
-}
-
-
-#[test_case]
-fn trivial_asseration(){
-        // println!("TEST 1| TEST RESULT -->");
-        // assert_eq!(1,1);
-        // println!("PASS")
-
-        //serial_print!("Triv assersation\n");
-        assert_eq!(1,1);
-        //serial_println!(" PASS");
+        FeOSMK1::test_panic_handler(info)
 }
